b7d1d4c33ac65a6de7935f4fa9f44bc9873e8ee1
Marco Barisione <marco@barisione.org>
2017-09-09 21:11:14 +0100

    Initial Commit

---
+++ WheresMySound/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  WheresMySound
+//
+//  Created by Marco Barisione on 09/09/2017.
+//  Copyright © 2017 Marco Barisione. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
