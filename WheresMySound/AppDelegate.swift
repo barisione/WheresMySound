8eaaefdc2910ebf30309eddf7d4987b26a701109
Marco Barisione <marco@barisione.org>
2017-09-16 10:03:53 +0100

    Animate the icon when the output source changes

--- WheresMySound/AppDelegate.swift
+++ WheresMySound/AppDelegate.swift
@@ -14,6 +14,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     let statusItem = NSStatusBar.system().statusItem(withLength: NSSquareStatusItemLength)
     var currentDeviceMenuItem: NSMenuItem?
     var watcher = SoundDeviceWatcher()
+    var iconUpdateTimer: Timer?
 
     #if DEBUG
     var cyclingIconsTimer: Timer?
@@ -27,6 +28,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
     func applicationWillTerminate(_ aNotification: Notification) {
         self.watcher.stopListening()
+        self.stopIconAnimation()
 
         #if DEBUG
             self.cyclingIconsTimer?.invalidate()
@@ -54,10 +56,88 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         statusItem.menu = menu
     }
 
-    func outputSourceChanged(deviceType: AudioDeviceType) {
+    private var sourceHasChangedBefore = false
+
+    private func outputSourceChanged(deviceType: AudioDeviceType) {
+        // We don't animate the initial style after starting the program.
+        self.updateOutputSourceIcon(deviceType: deviceType, animate: self.sourceHasChangedBefore)
+        self.sourceHasChangedBefore = true
+    }
+
+    private func updateOutputSourceIcon(deviceType: AudioDeviceType,
+                                        animate: Bool) {
         print("Sound coming from \(deviceType)")
-        statusItem.button?.image = deviceType.icon
+
         self.currentDeviceMenuItem?.title = "Default output: \(deviceType.displayName)"
+
+        if animate {
+            self.startIconAnimation(deviceType: deviceType)
+        } else {
+            self.setFinalIcon(deviceType: deviceType)
+        }
+    }
+
+    private func setFinalIcon(deviceType: AudioDeviceType) {
+        self.statusItem.button?.image = deviceType.icon
+    }
+
+    private func stopIconAnimation() {
+        self.iconUpdateTimer?.invalidate()
+        self.iconUpdateTimer = nil
+    }
+
+    private func startIconAnimation(deviceType: AudioDeviceType) {
+        self.stopIconAnimation()
+
+        var currentIteration = 0
+        let template = deviceType.icon
+
+        // We create several tintend images to display an animation.
+        // The gradient gives us the steps in colour we need.
+        let startColor = self.systemIsUsingDarkTheme() ? NSColor.white : NSColor.black
+        let gradient = NSGradient(starting:startColor,
+                                  ending:NSColor(red:0, green:0.75, blue:1, alpha:1))!
+
+        var allColoredIcons = [NSImage]()
+        // First generate a few steps.
+        for step in stride(from: 0.0, through: 1.0, by: 0.05) {
+            let color = gradient.interpolatedColor(atLocation: CGFloat(step))
+            allColoredIcons.append(template.tintedTemplate(color: color))
+        }
+        // Then make the last step persist a bit longer (it just looks nicer).
+        let lastStep = allColoredIcons[allColoredIcons.count - 1]
+        for _ in 0..<5 {
+            allColoredIcons.append(lastStep)
+        }
+        // And finally duplicate the same images but in reverse order (to show the colour going back).
+        for i in stride(from: allColoredIcons.count - 1, through: 0, by: -1) {
+            allColoredIcons.append(allColoredIcons[i])
+        }
+
+        self.iconUpdateTimer = Timer.scheduledTimer(withTimeInterval: 0.05, repeats: true) {
+            (_) in
+
+            let index = currentIteration % allColoredIcons.count
+            currentIteration += 1
+
+            if index == 0 && currentIteration / allColoredIcons.count >= 4 {
+                // Animated for long enough.
+                self.stopIconAnimation()
+                self.setFinalIcon(deviceType: deviceType)
+                return
+            }
+
+            self.statusItem.button?.image = allColoredIcons[index]
+        }
+    }
+
+    func systemIsUsingDarkTheme() -> Bool {
+        let style = UserDefaults.standard.string(forKey: "AppleInterfaceStyle")
+        if let style = style, style == "Dark" {
+            return true
+        } else {
+            return false
+        }
     }
 
     #if DEBUG
@@ -87,7 +167,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         var currentIndex = 0;
         self.cyclingIconsTimer = Timer.scheduledTimer(withTimeInterval: 4, repeats: true) {
             (_) in
-            self.outputSourceChanged(deviceType: allTypes[currentIndex])
+            self.updateOutputSourceIcon(deviceType: allTypes[currentIndex], animate: false)
             currentIndex = (currentIndex + 1) % allTypes.count
         }
 
---
+++ WheresMySound/NSImage+Tinting.swift
@@ -0,0 +1,23 @@
+//
+//  NSImage+Tinting.swift
+//  WheresMySound
+//
+//  Created by Marco Barisione on 16/09/2017.
+//  Copyright © 2017 Marco Barisione. All rights reserved.
+//
+
+import Cocoa
+
+extension NSImage {
+    func tintedTemplate(color: NSColor) -> NSImage {
+        let tinted = self.copy() as! NSImage
+        tinted.isTemplate = false
+        tinted.lockFocus()
+        color.set()
+        let rect = NSMakeRect(0, 0, self.size.width, self.size.height)
+        NSRectFillUsingOperation(rect, .sourceAtop)
+        tinted.unlockFocus()
+
+        return tinted
+    }
+}
